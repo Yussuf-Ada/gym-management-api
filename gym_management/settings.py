9456cc27c370ccee3ef28d3874a416f73c47c269
Yussuf-Ada <yussuf.shariff@ada.ac.uk>
2026-03-25 18:09:19 +0000

    Add SendGrid emails and Cloudinary image uploads

--- gym_management/settings.py
+++ gym_management/settings.py
@@ -164,3 +164,18 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# SendGrid Settings
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'apikey'
+# EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')
+# DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+# Use SendGrid Python library instead
+EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
--- users/utils.py
+++ users/utils.py
@@ -1,29 +1,43 @@
 import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from django.core.mail import send_mail
 
 
 def send_password_reset_email(email, reset_token):
     frontend_url = os.getenv('FRONTEND_URL', 'http://localhost:5173')
     reset_link = f"{frontend_url}/reset-password?token={reset_token}"
 
-    message = Mail(
-        from_email=os.getenv('DEFAULT_FROM_EMAIL', 'noreply@gym.com'),
-        to_emails=email,
-        subject='Password Reset Request',
-        html_content=f'''
-            <h2>Password Reset</h2>
-            <p>You requested a password reset for your Gym Management account.</p>
-            <p>Click the link below to reset your password:</p>
-            <p><a href="{reset_link}">Reset Password</a></p>
-            <p>This link will expire in 1 hour.</p>
-            <p>If you didn't request this, please ignore this email.</p>
-        '''
-    )
+    subject = 'Password Reset Request'
+    message = f'''
+        Password Reset
+        
+        You requested a password reset for your Gym Management account.
+        
+        Click the link below to reset your password:
+        {reset_link}
+        
+        This link will expire in 1 hour.
+        
+        If you didn't request this, please ignore this email.
+    '''
+    html_message = f'''
+        <h2>Password Reset</h2>
+        <p>You requested a password reset for your Gym Management account.</p>
+        <p>Click the link below to reset your password:</p>
+        <p><a href="{reset_link}">Reset Password</a></p>
+        <p>This link will expire in 1 hour.</p>
+        <p>If you didn't request this, please ignore this email.</p>
+    '''
 
     try:
-        sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
-        sg.send(message)
+        send_mail(
+            subject,
+            message,
+            os.getenv('DEFAULT_FROM_EMAIL', 'noreply@gym.com'),
+            [email],
+            html_message=html_message,
+            fail_silently=False,
+        )
         return True
-    except Exception:
+    except Exception as e:
+        print(f"Failed to send password reset email: {e}")
         return False
