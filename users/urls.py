bdc4068459c49467d04007f7c0048b0ba90f07ee
Yussuf-Ada <yussuf.shariff@ada.ac.uk>
2026-03-26 23:53:13 +0000

    Restore mock data functionality for video demo

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, LoginView, LogoutView, UserProfileView, ChangePasswordView, DashboardStatsView, PasswordResetRequestView, PasswordResetConfirmView, RecentActivityView #, LoadMockDataView
+from .views import RegisterView, LoginView, LogoutView, UserProfileView, ChangePasswordView, DashboardStatsView, PasswordResetRequestView, PasswordResetConfirmView, RecentActivityView, LoadMockDataView
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
@@ -13,5 +13,5 @@ urlpatterns = [
     path('users/me/change-password/', ChangePasswordView.as_view(), name='change_password'),
     path('dashboard/stats/', DashboardStatsView.as_view(), name='dashboard_stats'),
     path('dashboard/activity/', RecentActivityView.as_view(), name='recent_activity'),
-    # path('load-mock-data/', LoadMockDataView.as_view(), name='load_mock_data'),
+    path('load-mock-data/', LoadMockDataView.as_view(), name='load_mock_data'),
 ]
