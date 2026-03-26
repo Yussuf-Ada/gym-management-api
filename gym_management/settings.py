9ccb835e83172bd6fe7d4137dd9acfda1c86dd1e
Yussuf-Ada <yussuf.shariff@ada.ac.uk>
2026-03-26 21:35:48 +0000

    Fix SendGrid backend import path

--- gym_management/settings.py
+++ gym_management/settings.py
@@ -166,6 +166,6 @@ CLOUDINARY_STORAGE = {
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # SendGrid Settings
-EMAIL_BACKEND = 'sgbackend.SendGridBackend'
+EMAIL_BACKEND = 'sendgrid_backend.SendGridBackend'
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
