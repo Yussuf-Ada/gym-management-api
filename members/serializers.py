fd89b320252591b7fa48b6d23b64e28365cce130
Yussuf-Ada <yussuf.shariff@ada.ac.uk>
2026-03-27 14:46:04 +0000

    Add date_of_birth and emergency_contact to MemberListSerializer

--- members/serializers.py
+++ members/serializers.py
@@ -44,7 +44,7 @@ class MemberListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Member
-        fields = ('id', 'full_name', 'email', 'phone', 'profile_image_url', 'is_active', 'joined_date')
+        fields = ('id', 'full_name', 'email', 'phone', 'date_of_birth', 'emergency_contact', 'profile_image_url', 'is_active', 'joined_date')
     
     def get_profile_image_url(self, obj):
         if obj.profile_image:
